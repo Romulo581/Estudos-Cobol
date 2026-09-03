f4770f31aac2af900ea5f735b051393976b00e31
Rômulo César Souza Alves da Silva <151069888+Romulo581@users.noreply.github.com>
2026-09-03 14:49:41 -0300

    Programa Wello World

---
+++ Helloworld.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+           PROGRAM-ID. WELLOWORLD.
+           AUTHOR. ROMULO CESAR
+       ENVIRONMENT DIVISION.
+      
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+       PRINCIPAL.
+           DISPLAY "Hello World".
+
+       STOP RUN.
+       END PROGRAM WELLOWORLD.
+       
