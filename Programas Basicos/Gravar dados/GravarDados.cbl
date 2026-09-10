24f0507a60afd5662738ebe88a994084f4c59b2b
Rômulo César Souza Alves da Silva <151069888+Romulo581@users.noreply.github.com>
2026-09-10 13:01:47 -0300

    Definindo campos do arquivo.

--- Programas Basicos/Gravar dados/GravarDados.cbl
+++ Programas Basicos/Gravar dados/GravarDados.cbl
@@ -19,3 +19,22 @@
 
            SELECT NOTA-QUINTO-B ASSIGN TO "NotasBimentre5"
                ORGANIZATION IS LINE SEQUENTIAL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD  NOTAS-PRIMEIRO-B
+       01  DET-B-01
+           88 FINALIZARBIMESTRE VALUER HIGH-VALUES.
+           05 MATRICULA-ALUNO               PIC 9(006).
+           05 NOME-ALUNO.
+               10  PRIMEIRO-NOME            PIC X(020).
+               10  SEGUNDO-NOME             PIC X(020).
+           05  GENERO                       PIC A(001).
+           05  NOTA-B-01.
+               10  NOTA-INTEIRA             PIC 9(002).
+               FILLER                       PIC X(001) VALUE ",".
+               10  NOTA-DECIMAL             PIC 9(002).
+       FD  NOTAS-SEGUNDO-B
+       FD  NOTAS-TERCEIRO-B
+       FD  NOTAS-QUANTO-B
+       FD  NOTA-QUINTO-B
