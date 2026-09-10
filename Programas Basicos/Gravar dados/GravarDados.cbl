68f4b58e5098abd626020eff45e3b96dffc33a18
Rômulo César Souza Alves da Silva <151069888+Romulo581@users.noreply.github.com>
2026-09-10 15:57:48 -0300

    Construindo a logica paga gravar dados em um arquivo, com uma verificação de nome e de matricula, com media da notas

--- Programas Basicos/Gravar dados/GravarDados.cbl
+++ Programas Basicos/Gravar dados/GravarDados.cbl
@@ -85,36 +85,112 @@
        WORKING-STORAGE.
        01  NOTA-FORM-B-01.
            05  WS-NOTA-B-01.
-               10 WS-NOTA-INTEIRA-B01              PIC 9(002) VALUE 0.   
+               10 WS-NIT-B01                       PIC 9(002) VALUE 0.   
                FILLER                              PIC A(001) VALUE ",".
-               10 WS-NOTA-DECIMAL-B01              PIC 9(002) VALUE 0.
+               10 WS-NDC-B01                       PIC 9(002) VALUE 0.
 
        01  NOTA-FORM-B-02.
            05  WS-NOTA-B-02.
-               10 WS-NOTA-INTEIRA-B02              PIC 9(002) VALUE 0.   
+               10 WS-NIT-B02                       PIC 9(002) VALUE 0.   
                FILLER                              PIC A(001) VALUE ",".
-               10 WS-NOTA-DECIMAL-B02              PIC 9(002) VALUE 0.       
+               10 WS-NDC-B02                       PIC 9(002) VALUE 0.       
 
        01  NOTA-FORM-B-03.
            05  WS-NOTA-B-03.
-               10 WS-NOTA-INTEIRA-B03              PIC 9(002) VALUE 0.   
+               10 WS-NIT-B03                       PIC 9(002) VALUE 0.   
                FILLER                              PIC A(001) VALUE ",".
-               10 WS-NOTA-DECIMAL-B03              PIC 9(002) VALUE 0. 
+               10 WS-NDC-B03                       PIC 9(002) VALUE 0. 
 
        01  NOTA-FORM-B-04.
            05  WS-NOTA-B-04.
-               10 WS-NOTA-INTEIRA-B04              PIC 9(002) VALUE 0.          
+               10 WS-NIT-B04                       PIC 9(002) VALUE 0.          
                FILLER                              PIC A(001) VALUE ",".
-               10 WS-NOTA-DECIMAL-B04              PIC 9(002) VALUE 0.
+               10 WS-NDC-B04                       PIC 9(002) VALUE 0.
        
        01  NOTA-FORM-B-05.
            05  WS-NOTA-B-05.
-               10 WS-NOTA-INTEIRA-B05              PIC 9(002) VALUE 0.           
+               10 WS-NIT-B05                       PIC 9(002) VALUE 0.           
                FILLER                              PIC A(001) VALUE ",".
-               10 WS-NOTA-DECIMAL-B05              PIC 9(002) VALUE 0.
+               10 WS-NDC-B05                       PIC 9(002) VALUE 0.
+
+       01  WS-NA-B01.                              PIC X(020) VALUE " ".
+       01  WS-NA-B02.                              PIC X(020) VALUE " ".
+       01  WS-NA-B03.                              PIC X(020) VALUE " ".
+       01  WS-NA-B04.                              PIC X(020) VALUE " ".
+       01  WS-NA-B05.                              PIC X(020) VALUE " ".
+       
+       01  WS-NOME-IGUAL                           PIC X(001) VALUE "N".
 
        01  LEITURA-B01-FINALIZADA                  PIC X VALUE "N".
        01  LEITURA-B02-FINALIZADA                  PIC X VALUE "N".
        01  LEITURA-B03-FINALIZADA                  PIC X VALUE "N".
        01  LEITURA-B04-FINALIZADA                  PIC X VALUE "N".
        01  LEITURA-B05-FINALIZADA                  PIC X VALUE "N".
+
+       PROCEDURE DIVISION.
+       INICIO.
+           PERFORM HEADER.
+
+           OPEN INPUT NOTA-PRIMEIRO-B.
+           PERFORM PROCESSAR-REG-B01.
+           CLOSE NOTA-PRIMEIRO-B.
+
+           OPEN INPUT NOTA-SEGUNDO-B.
+           PERFORM PROCESSAR-REG-B02.
+           CLOSE NOTA-SEGUNDO-B.
+
+           OPEN INPUT NOTA-TERCEIRO-B.
+           PERFORM PRECESSAR-REG-B03.
+           CLOSE NOTA-TERCEIRO-B.
+
+           OPEN INPUT NOTA-QUARTO-B.
+           PERFORM PROCESSAR-REG-B04.
+           CLOSE NOTA-QUARTO-B.
+
+           OPEN INPUT NOTA-QUINTO-B.
+           PERFORM PROCESSAR-REG-B05.
+           CLOSE NOTA-QUINTO-B.
+
+           PERFORM EXIBIR-RESUMO.
+
+       STOP RUN.
+
+       HEADER.
+       DISPLAY "=====================================================".
+       DISPLAY "|_________ Media escolar Anual dos alunos __________|".
+       DISPLAY "=====================================================".
+
+       PROCESSAR-REG-B01.
+           PERFORM UNTIL LEITURA-B01-FINALIZADA = "S"
+               PERFORM LER-REG-B01
+               IF LEITURA-B01-FINALIZADA = "N"
+                   PERFORM ERRO-LEITURA-B01
+               END-IF
+           END-PERFORM.
+
+       LER-REG-B01.
+           READ NOTA-PRIMEIRO-B INTO DET-B-01
+               AT END
+                   MOVE "S" TO LEITURA-B01-FINALIZADA
+               NOT AT END
+                   MOVE NOTA-INTEIRA-B01 TO WS-NIT-B01
+                   MOVE NOTA-DECIMAL-B01 TO WS-NDC-B01
+                   MOVE NOME-ALUNO-B01   TO WS-NA-B01
+                   CONTINUE
+           END-READ.
+       ERRO-LEITURA-B01.
+           DISPLAY "Erro na leitura do Arquivo NotasBimestre1".
+
+       EXIBIR-RESUMO.
+           PERFORM CALC
+
+       CALC.
+           IF WS-NA-B01 = WS-NA-B02
+               AND WS-NA-B02 = WS-NA-B03
+               AND WS-NA-B03 = WS-NA-B04
+               AND WS-NA-B04 = WS-NA-B05
+           
+               MOVE "S" WS-NOME-IGUAL
+           ELSE
+               MOVE "N" WS-NOME-IGUAL
+           END-IF
