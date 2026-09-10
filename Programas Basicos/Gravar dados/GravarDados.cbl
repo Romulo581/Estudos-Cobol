2380bd6977b959f10a953b6fcf2314157c1de3fd
Rômulo César Souza Alves da Silva <151069888+Romulo581@users.noreply.github.com>
2026-09-10 14:06:28 -0300

    As Variaveis dos arquivos foram definidas e configuradas, para suportar o tamano correto.

--- Programas Basicos/Gravar dados/GravarDados.cbl
+++ Programas Basicos/Gravar dados/GravarDados.cbl
@@ -5,16 +5,16 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL 
-           SELECT NOTAS-PRIMEIRO-B ASSIGN TO  "NotasBimestre1"
+           SELECT NOTA-PRIMEIRO-B ASSIGN TO  "NotasBimestre1"
                ORGANIZATION IS LINE SEQUENTIAL.
            
-           SELECT NOTAS-SEGUNDO-B ASSIGN TO "NotasBimestre2"
+           SELECT NOTA-SEGUNDO-B ASSIGN TO "NotasBimestre2"
                ORGANIZATION IS LINE SEQUENTIAL.
 
-           SELECT NOTAS-TERCEIRO-B ASSIGN TO "NotasBimentre3"
+           SELECT NOTA-TERCEIRO-B ASSIGN TO "NotasBimentre3"
                ORGANIZATION IS LINE SEQUENTIAL.
 
-           SELECT NOTAS-QUANTO-B ASSIGN TO "NotasBimentre4"
+           SELECT NOTA-QUARTO-B ASSIGN TO "NotasBimentre4"
                ORGANIZATION IS LINE SEQUENTIAL.
 
            SELECT NOTA-QUINTO-B ASSIGN TO "NotasBimentre5"
@@ -22,19 +22,64 @@
 
        DATA DIVISION.
        FILE SECTION.
-       FD  NOTAS-PRIMEIRO-B
-       01  DET-B-01
-           88 FINALIZARBIMESTRE VALUER HIGH-VALUES.
-           05 MATRICULA-ALUNO               PIC 9(006).
-           05 NOME-ALUNO.
-               10  PRIMEIRO-NOME            PIC X(020).
-               10  SEGUNDO-NOME             PIC X(020).
-           05  GENERO                       PIC A(001).
+       FD  NOTA-PRIMEIRO-B.
+       01  DET-B-01.
+           88 FIN-B-01  VALUE                  HIGH-VALUES.
+           05 MATRICULA-ALUNO-B01              PIC 9(006).
+           05 NOME-ALUNO-B01.
+               10  PRIMEIRO-NOME-B01           PIC X(020).
+               10  SEGUNDO-NOME-B01            PIC X(020).
+           05  GENERO-B01                      PIC A(001).
            05  NOTA-B-01.
-               10  NOTA-INTEIRA             PIC 9(002).
-               FILLER                       PIC X(001) VALUE ",".
-               10  NOTA-DECIMAL             PIC 9(002).
-       FD  NOTAS-SEGUNDO-B
-       FD  NOTAS-TERCEIRO-B
-       FD  NOTAS-QUANTO-B
-       FD  NOTA-QUINTO-B
+               10  NOTA-INTEIRA-B01            PIC 9(002).
+               10  NOTA-DECIMAL-B01            PIC 9(002).
+
+       FD  NOTA-SEGUNDO-B.
+       01  DET-B-02.
+           88 FIN-B-02 VALUE                   HIGH-VALUES.
+           05 MATRICULA-ALUNO-B02              PIC 9(006).
+           05 NOME-ALUNO-B02.
+               10 PRIMEIRO-NOME-B02             PIC X(020).
+               10 SEGUNDO-NOME-B02             PIC X(020).
+           05  GENERO-B02                      PIC A(001).
+           05  NOTA-B-02.
+               10 NOTA-INTEIRA-B02             PIC 9(002).
+               10 NOTA-DECIMAL-B02             PIC 9(002).
+
+       FD  NOTA-TERCEIRO-B.
+       01  DET-B-03.
+           88 FIN-B-03 VALUE                   HIGH-VALUES.
+           05 MATRICULA-ALUNO-B03              PIC 9(006).
+           05 NOME-ALUNO-B03.
+               10 PRIMEIRO-NOME-B03            PIC X(020).
+               10 SEGUNDO-NOME-B03             PIC X(020).
+           05  GENERO-B03                      PIC A(001).
+           05 NOTA-B-03.
+               10 NOTA-INTEIRA-B03             PIC 9(002).
+               10 NOTA-DECIMAL-B03             PIC 9(002).
+
+       FD  NOTA-QUARTO-B.
+       01  DET-B-04.
+           88 FIN-B-04 VALUE                   HIGH-VALUES.
+           05  MATRICULA-ALUNO-B04             PIC 9(006).
+           05  NOME-ALUNO-B04.
+               10 PRIMEIRO-NOME-B04            PIC X(020).
+               10 SEGUNDO-NOME-B04             PIC X(020).
+           05 GENERO-B04                       PIC A(001).
+           05 NOTA-B-04.
+               10 NOTA-INTEIRA-B04             PIC 9(002).
+               10 NOTA-DECIMAL-B04             PIC 9(002).
+
+       FD  NOTA-QUINTO-B.
+       01  DET-B-05.
+           88 FIN-B-05 VALUE                   HIGH-VALUES.
+           05 MATRICULA-ALUNO-B05              PIC 9(006).
+           05 NOME-ALUNO-B05.
+               10 PRIMEIRO-NOME-B05            PIC X(020).
+               10 SEGUNDO-NOME-B05             PIC X(020).
+           05 GENERO-B05                       PIC A(001).
+           05 NOTA-B-05.
+               10 NOTA-INTEIRA-B05             PIC 9(002).
+               10 NOTA-DECIMAL-B05             PIC 9(002).
+       WORKING-STORAGE.
+       
